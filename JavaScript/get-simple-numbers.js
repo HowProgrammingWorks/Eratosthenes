5e7123a59cd247234d9dbc3e70dc79ece820fd76
burbokop <burbokop@gmail.com>
2018-09-23 13:24:51 +0300

    modifued ./JavaSkript/get-simple-numbers.js

--- JavaScript/get-simple-numbers.js
+++ JavaScript/get-simple-numbers.js
@@ -3,11 +3,13 @@
 const sort = (array, divider) => {
   console.log('divider: ' + divider);
   let erased = 0;
-  for (const value of array) if (value % divider === 0 && value !== divider) {
-    array.splice(array.indexOf(value), 1);
-    array.splice(array.indexOf(divider), 1);
-    array.push(divider);
-    erased++;
+  for (const value of array) {
+    if (value % divider === 0 && value !== divider) {
+      array.splice(array.indexOf(value), 1);
+      array.splice(array.indexOf(divider), 1);
+      array.push(divider);
+      erased++;
+    }
   }
 
   console.log('array after erasing: ' + array);
@@ -21,10 +23,12 @@ const getArrayOfSimpleNumbers = range => {
   return sort(array, array[0]);
 };
 
-if (process.argv.length > 2)
+if (process.argv.length > 2) {
   console.log('\nresult: ' + getArrayOfSimpleNumbers(process.argv[2]));
-else {
+} else {
   console.log('Program allows getting simple number');
   console.log('Sieve of Eratosthenes is used.');
   console.log('usage:\n node ./get-simple-numbers.js [renge of numbers]\n');
+  console.log('example:\n node ./get-simple-numbers.js 100\n');
+
 }
