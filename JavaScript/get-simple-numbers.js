76fbca51258ec954eb3b1b1c06ea033064facfae
burbokop <burbokop@gmail.com>
2018-09-24 09:03:05 +0300

    recursion changed to loop in get-simple-numbers.js

--- JavaScript/get-simple-numbers.js
+++ JavaScript/get-simple-numbers.js
@@ -1,26 +1,26 @@
 'use strict';
 
-const sort = (array, divider) => {
-  console.log('divider: ' + divider);
-  let erased = 0;
-  for (const value of array) {
-    if (value % divider === 0 && value !== divider) {
-      array.splice(array.indexOf(value), 1);
-      array.splice(array.indexOf(divider), 1);
-      array.push(divider);
-      erased++;
-    }
-  }
-
-  console.log('array after erasing: ' + array);
-  if (erased === 0) return array.sort((a, b) => a - b);
-  return sort(array, array[0]);
-};
-
 const getArrayOfSimpleNumbers = range => {
   const array = [];
   for (let i = 2; i < range; i++) array.push(i);
-  return sort(array, array[0]);
+
+  let erased;
+  while (erased !== 0) {
+    const divider = array[0];
+    erased = 0;
+    process.stdout.write('divider: ' + divider + ' ');
+    for (const value of array) {
+      if (value % divider === 0 && value !== divider) {
+        array.splice(array.indexOf(value), 1);
+        array.splice(array.indexOf(divider), 1);
+        array.push(divider);
+        erased++;
+      }
+    }
+
+    console.log('array after erasing: ' + array);
+  }
+  return array;//.sort((a, b) => a - b)
 };
 
 if (process.argv.length > 2) {
